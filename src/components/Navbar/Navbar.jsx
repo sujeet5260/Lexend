192d635a16f9e65d0f2991989c1f58464bea945f
sujeet yadav <sujigyadav595@gmail.com>
2025-04-17 12:34:19 +0530

    Navbar created

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoIosArrowDown } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="uc-navbar container mx-auto min-h-[4rem] flex justify-between items-center px-4 xl:justify-around ">
+      {/* Logo image  */}
+      <div className="uc-navbar-left flex gap-16">
+        <img
+          src="https://lexend-nextjs.vercel.app/assets/images/common/logo-light.svg"
+          alt=""
+        />
+
+        <div className="uc-navbar-nav hidden lg:flex items-center gap-6 [&>*]:flex [&>*]:items-center [&>*]:gap-1  [&>*]:font-semibold text-base xl:gap-10">
+          <Link to=''>Features <IoIosArrowDown />
+          </Link>
+          <Link to=''>Products <IoIosArrowDown /></Link>
+          <Link to=''>Resources <IoIosArrowDown /></Link>
+          <Link to=''>Pricing </Link>
+          <Link to=''>More</Link>
+        </div>
+      </div>
+      {/* Hamburger-Menu */}
+      <div className="uc-navbar-right lg:flex items-center gap-7 ">
+        <RxHamburgerMenu size={23} className="font-bold lg:hidden" />
+        <ul className=" hidden lg:flex">
+          <Link className="font-semibold text-base">Request a demo</Link>
+        </ul>
+        <button
+          type="button"
+          className="hidden lg:flex text-base font-semibold bg-[#12715B] text-white rounded-lg py-3 px-3 leading-none cursor-pointer hover:bg-opacity-90"
+        >
+          Start free trial
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
